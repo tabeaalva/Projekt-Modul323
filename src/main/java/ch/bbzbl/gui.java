b007043d787d2140de841b824a929eff603deb97
MrGowi <josegonzalezwillems@gmail.com>
2023-10-17 15:15:26 +0200

    GUI Klasse, anzeige in der Konsole

---
+++ src/main/java/ch/bbzbl/gui.java
@@ -0,0 +1,39 @@
+public class gui {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("\n=== Sterbedaten-Analyse-Menü ===");
+        System.out.println("1. Filtern nach Wochentag");
+        System.out.println("2. Höchster Wert der Wochen eines Jahres");
+        System.out.println("3. Gesamtzahl der Todesfälle in einem Jahr");
+        System.out.println("4. Beenden");
+        System.out.print("Bitte wählen Sie eine Option (1/2/3/4): ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Verbrauche das Zeilenende nach der Eingabe.
+
+        switch (choice) {
+            case 1:
+                System.out.print("Geben Sie den gewünschten Wochentag ein (z.B. 'Mi'): ");
+                String targetWochentag = scanner.nextLine();
+                filterByWochentag();
+                break;
+            case 2:
+                System.out.print("Geben Sie das Jahr ein, für das Sie den höchsten Wochenwert ermitteln möchten: ");
+                int year = scanner.nextInt();
+                findMaxWeekValueForYear();
+                break;
+            case 3:
+                System.out.print("Geben Sie das Jahr ein, für das Sie die Gesamtzahl der Todesfälle ermitteln möchten: ");
+                int targetYear = scanner.nextInt();
+                calculateTotalDeathsForYear();
+                break;
+            case 4:
+                System.out.println("Das Programm wird beendet.");
+                scanner.close();
+                System.exit(0);
+            default:
+                System.out.println("Ungültige Auswahl. Bitte wählen Sie eine der verfügbaren Optionen (1/2/3/4).");
+        }
+
+}
