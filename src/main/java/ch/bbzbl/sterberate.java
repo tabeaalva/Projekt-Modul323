d55752019c5c5a71bcd6a93453e9b1ece6228ad6
Tabea Reiffer <tr@cyon.ch>
2023-10-17 14:55:45 +0200

    starting project and adding classes

---
+++ src/main/java/ch/bbzbl/sterberate.java
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+// Start of project
+=======
+public class Sterberate {
+ private String jahr;
+ private int monat;
+ private String wochentag;
+ private String sterberate;
+ private String anzTotal;
+
+public Book(String jahr, int monat) {
+    this.jahr = jahr;
+    this.monat = monat;
+}
+ 
+public void setJahr(String jahr) {
+    this.jahr = jahr;
+}
+
+public int getJahr() {
+    return jahr;
+}
+
+ // Konstruktor, Getter und Setter hier...
+
+ // Stelle sicher, dass du einen Standardkonstruktor ohne Argumente für Gson hast
+ public Benutzer() {
+ }
+}
+>>>>>>> e933277 (start sterberate klass)
